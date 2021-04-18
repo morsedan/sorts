6c4c2134f8d00f1ec629cf68ad6583ecbbc6e4a3
ahavamorse <ahavamorse@icloud.com>
2021-04-18 09:54:10 -0700

    In class notes on quick sort, code and runtime

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,9 +1,49 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
+# Quick Sort Steps
+    # Choose a pivot
+    # Create new array with pivot as only element
+    # Create an array of elements less than pivot on the left
+    # Create an array of elements greater than pivot on the right
+    # Repeat with arrays on left and right
+    # Combine sorted arrays and return a single sorted array
+
+def quicksort(numbers):  # perfect: O(n log n)  worst: n^2
+    # perfect: because it goes log(n) levels (dividing by two each time)
+    # with n at each level (due to the partition) so doing n operations log(n) times
+    # Assuming the array is pretty balanced (being split in half each time)
+
+    # average: because you choose the smallest or biggest element each time
+    # which means you are doing n operations n times (with no dividing each time)
+
+    # base case (conquer)
+    if len(numbers) <= 1:
+        return numbers
+
+    # recursive case (divide)
+    left, pivot, right = partition(numbers)  # O(n)
+    sorted_array = quicksort(left) + pivot + quicksort(right)
+    return sorted_array
+
+
+def partition(numbers):  # Runs in O(n)
+    # helper function to split array into left, pivot, and right
+    left = []
+    pivot = [numbers[0]]
+    right = []
+
+    for num in numbers[1:]:  # Starting at index of 1 to skip pivot
+        if num <= pivot:
+            left.append(num)
+        else:
+            right.append(num)
+
+    return left, pivot, right
+
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge( arrA, arrB  ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+
     return merged_arr
 
 
