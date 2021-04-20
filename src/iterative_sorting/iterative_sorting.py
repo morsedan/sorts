b527a658a3ee5e98f463e163a7a52532446ec398
ahavamorse <ahavamorse@icloud.com>
2021-04-20 11:29:17 -0700

    Implemented linear search to find a target value in an array

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,13 @@
-# TO-DO: Complete the selection_sort() function below 
+# O(n) linear time
+def linear_search(arr, target):
+    for num in arr:
+        if num == target:
+            return True
+        return False
+
+
+
+# TO-DO: Complete the selection_sort() function below
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
